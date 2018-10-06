0b0abe039348019c555294474b7a1eb720f33f40
Gaetan <ligno19@gmail.com>
2018-10-06 16:26:49 -0400

    Activator

---
+++ JamNation2018_19_1/Assets/Scripts/Environment/Activators/PressButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressButton : ActivatorIN {
+
+    public float Sensibility;
+
+
+    private Vector3 basePos;
+    private bool trigger = true;
+
+
+    void Awake() {
+        basePos = transform.localPosition;
+    }
+
+    void Update() {
+        if (trigger) {
+            if (basePos.y - Sensibility > transform.localPosition.y ) {
+                trigger = false;
+                Activate();
+                Debug.Log("Pushed");
+            }
+        }
+    }
+
+
+}
