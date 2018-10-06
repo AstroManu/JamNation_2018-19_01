f28ee407fe1310a5d316dd43b75d3606d7dfc71f
Alexis <harvey.alexis@hotmail.com>
2018-10-06 13:51:08 -0400

    New Default Player

--- JamNation2018_19_1/Assets/Scripts/Players/BasePlayer.cs
+++ JamNation2018_19_1/Assets/Scripts/Players/BasePlayer.cs
@@ -8,10 +8,11 @@ public class BasePlayer : MonoBehaviour {
 	public string rewiredPlayer;
 	private Player player;
 
-	public TriggerArea triggerArea;
+	private TriggerArea triggerArea;
 	private TriggerZone groundCheckZone;
 
 	private Rigidbody rb;
+	private Transform modelPivot;
 
 	public float lateralForce = 10f;
 	public float airLateralForce = 5f;
@@ -19,11 +20,17 @@ public class BasePlayer : MonoBehaviour {
 	public float jumpImpulse = 10f;
 	public float gravityAcceleration = 5f;
 
+	private Quaternion targetRotation;
+
 	private void Start ()
 	{
 		player = ReInput.players.GetPlayer(rewiredPlayer);
+		triggerArea = GetComponent<TriggerArea>();
 		groundCheckZone = triggerArea.GetZone("GroundCheck");
 		rb = GetComponent<Rigidbody>();
+		modelPivot = transform.Find("Model");
+
+		targetRotation = modelPivot.rotation;
 	}
 	
 	private void FixedUpdate ()
@@ -31,6 +38,8 @@ public class BasePlayer : MonoBehaviour {
 		bool groundCheck = groundCheckZone.GetHits(LayerMask.GetMask("Ground", "Player")).Length > 0;
 
 		ApplyLateralInput(groundCheck);
+
+		RotatePlayerModel();
 
 		if (player.GetButtonDown("Jump"))
 		{
@@ -43,10 +52,26 @@ public class BasePlayer : MonoBehaviour {
 	protected virtual void ApplyLateralInput(bool groundCheck)
 	{
 		float moveForce = groundCheck ? lateralForce : airLateralForce;
-	
+
 		rb.AddForce(new Vector3(player.GetAxis("MoveHorizontal") * moveForce, 0f, 0f));
 
 		rb.velocity = new Vector3(Mathf.Clamp(rb.velocity.x, -maxLateralVelocity, maxLateralVelocity), rb.velocity.y, rb.velocity.z);
+	}
+
+	protected virtual void RotatePlayerModel ()
+	{
+		float moveInput = player.GetAxis("MoveHorizontal");
+
+		if (moveInput > 0.1f)
+		{
+			targetRotation = Quaternion.Euler(new Vector3(0f, 92f, 0f));
+		}
+		else if (moveInput < -0.1f)
+		{
+			targetRotation = Quaternion.Euler(new Vector3(0f, -92f, 0f));
+		}
+
+		modelPivot.rotation = Quaternion.RotateTowards(modelPivot.rotation, targetRotation, 15f);
 	}
 
 	protected virtual void ApplyJumpForce(bool groundCheck)
